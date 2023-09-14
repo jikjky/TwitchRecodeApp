0938f8c283126590cfd709691b3c58dc96d7bc41
lazymoon <lazymoon@twosun.com>
2023-09-14 15:20:27 +0900

    테스트용 코드 삭제

--- TwitchRecodeApp/Program.cs
+++ TwitchRecodeApp/Program.cs
@@ -9,33 +9,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Task.Run(async () =>
-        {
-            UserCredential credential;
-            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
-            {
-                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    // This OAuth 2.0 access scope allows for full read/write access to the
-                    // authenticated user's account.
-                    new[] { YouTubeService.Scope.Youtube },
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore("TwitchRecode")
-                );
-            }
-
-            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = "TwitchRecode"
-            });
-
-            var a = youtubeService.Playlists.List("");
-            var b = a.Execute();
-            
-        }).Wait();
-
         OSPlatform oSPlatform = new OSPlatform();
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
